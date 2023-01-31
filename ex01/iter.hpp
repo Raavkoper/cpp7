2ad9369a7d02c9a20c63c07bc297d081acb8e2eb
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2023-01-31 13:27:41 +0100

    started ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,12 @@
+#ifndef ITER_H
+#define ITER_H
+
+#include <iostream>
+
+template<typename T>
+void iter(T array[], size_t length, void (*func)(T&)) {
+    for (int i = 0; i < length; i++)
+        func(array[i]);
+}
+
+#endif
