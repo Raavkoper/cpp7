b3cb920b5ed4a5dc1acbc33b7dd305fcc80587d6
Raav Koper <rkoper@f1r5s18.codam.nl>
2023-02-06 12:13:58 +0100

    finished ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,14 @@
 #include "iter.hpp"
 
+template<typename T>
+void print_element(T element) {
+	std::cout << element << std::endl;
+}
+
 int main(void) {
-    
+    int intarray[10] = {3, 6346, 3432, 64, 123, 1235, 3462, 76, 7946, 68};
+	std::string stringarray[6] = {"what", "the", "fuck", "is", "up", "kyle"};
+
+	::iter(intarray, 10, print_element);
+	::iter(stringarray, 6, print_element);
 }
