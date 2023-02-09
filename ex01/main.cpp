dc547197c87f9e4239e2a1d39e2cacf619758bdc
Raav Koper <rkoper@f0r5s17.codam.nl>
2023-02-09 21:00:40 +0100

    pa

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,7 +7,7 @@ void print_element(T element) {
 
 int main(void) {
     int intarray[10] = {3, 6346, 3432, 64, 123, 1235, 3462, 76, 7946, 68};
-	std::string stringarray[6] = {"what", "the", "fuck", "is", "up", "kyle"};
+	std::string stringarray[4] = {"what's", "poppin", "my", "guy"};
 
 	::iter(intarray, 10, print_element);
 	::iter(stringarray, 6, print_element);
