21b51c45881c33ad63545013462181750acd9c05
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2023-02-09 15:13:54 +0100

    finished ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -11,12 +11,39 @@ private:
 
 public:
 	Array(): size(0),  array(0) {};
-	Array(unsigned int new_size): size(new_size) {};
+	Array(unsigned int new_size): size(new_size), array(new T[size]) {};
 	Array(const Array &other) {
-		size = other.size();
+		size = other.size;
+		array = new T[size];
+		for (int i = 0; i < size; i++)
+			array[i] = other.array[i];
 	}
+
 	Array &operator=(const Array &other) {
-		*this = other;
+		size = other.size;
+		delete[] array;
+		array = new T[size];
+		for (int i = 0; i < size; i++)
+			array[i] = other.array[i];
+		
+		return *this;
+	}
+
+	T &operator[](unsigned int index) {
+		if (index >= size) throw std::out_of_range("Index out of range");
+		else return (array[index]);
+	}
+
+	unsigned int getSize() { return size; }
+
+	void printArray() {
+		for (int i = 0; i < size; i++) {
+			std::cout << array[i] << std::endl;
+		}
+	}
+
+	~Array() {
+		delete[] array;
 	}
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,23 @@
 #include "Array.hpp"
 
 int main() {
-	Array<std::string> a(10);
-	std::string array[10];
-	Array b = a;
+	Array<int> a(10);
+	
+	for (int i = 0; i < 10; i++) {
+		a[i] = i;
+	}
+
+	Array<int> b(a);
+
+	b.printArray();
+
+	std::cout << std::endl;
+	std::cout << b.getSize() << std::endl;
+	std::cout << std::endl;
+
+	try {
+		std::cout << b[15] << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 }
