c6bb5308ec559b941947cf2df6e37b02c54163d3
Raav Koper <rkoper@f1r2s15.codam.nl>
2023-03-13 16:01:27 +0100

    fixed a thing

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,7 +4,12 @@
 #include <iostream>
 
 template<typename T>
-void iter(T array[], size_t length, void (*func)(T&)) {
+void print_element(const T element) {
+	std::cout << element << std::endl;
+}
+
+template<typename T>
+void iter(T array[], size_t length, void (*func)(const T&)) {
     for (int i = 0; i < length; i++)
         func(array[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,14 +1,9 @@
 #include "iter.hpp"
 
-template<typename T>
-void print_element(T element) {
-	std::cout << element << std::endl;
-}
-
 int main(void) {
     int intarray[10] = {3, 6346, 3432, 64, 123, 1235, 3462, 76, 7946, 68};
 	std::string stringarray[4] = {"what's", "poppin", "my", "guy"};
 
 	::iter(intarray, 10, print_element);
-	::iter(stringarray, 6, print_element);
+	::iter(stringarray, 4, print_element);
 }
